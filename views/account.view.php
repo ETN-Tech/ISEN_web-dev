b7a296c46c40060eb1db60ffc0182f1a1f19c8b3
ETN-Tech <etienne.schelfhout.pro@gmail.com>
2020-10-15 17:47:38 +0200

    Updated home, login, account and header views
    Included bootstrap elements

--- views/account.view.php
+++ views/account.view.php
@@ -10,24 +10,26 @@
 <body>
     <main>        
         <?php include('views/header.view.php'); ?>
-        
-        <div class="content">
-            <h2>Account details</h2>
-            <br>
-            <div class="row">
-                <div class="col-6">
-                    <div class="card border-light mb-3">
-                        <div class="card-header">Informations</div>
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $full_name; ?></h5>
-                            <p class="card-text"><b>Username :</b> <?php echo $account['username']; ?><br><b>Email :</b> <?php echo $account['email']; ?><br><b>Last connexion :</b> <?php echo $last_connexion; ?></p>
-                            
-                            <a href="account.php?logout" class="btn btn-danger">Logout</a>
+
+        <div class="container">
+            <div class="content">
+                <h2>Account details</h2>
+                <br>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card border-light mb-3">
+                            <div class="card-header">Informations</div>
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $full_name; ?></h5>
+                                <p class="card-text"><b>Username :</b> <?php echo $account['username']; ?><br><b>Email :</b> <?php echo $account['email']; ?><br><b>Last connexion :</b> <?php echo $last_connexion; ?></p>
+
+                                <a href="account.php?logout" class="btn btn-danger">Logout</a>
+                            </div>
                         </div>
                     </div>
                 </div>
+
             </div>
-            
         </div>
     </main>
     
