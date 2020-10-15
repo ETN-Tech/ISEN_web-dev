11902f1eb8aed7e5e7872aec816193fc73bad2a9
ETN-Tech <etienne.schelfhout.pro@gmail.com>
2020-10-15 21:31:52 +0200

    Fixed bdd-connexion

--- php/bdd-connexion.php
+++ php/bdd-connexion.php
@@ -2,9 +2,12 @@
 
 try
 {
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+
     $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-    
-    $bdd = new PDO('mysql:host=localhost; dbname=isen_bootstrap; charset=utf8','root','root');
+
+    $bdd = new PDO('pgsql:host='. $dbopts["host"] .';port='. $dbopts["port"] .';dbname='. ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"]);
+
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
