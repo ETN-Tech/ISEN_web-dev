32f0dfa99e9127ddb2b8e75c0c821700eb67e8d3
ETN-Tech <etienne.schelfhout.pro@gmail.com>
2020-11-06 14:26:25 +0100

    Updated Account class and account view

--- php/views/account.php
+++ php/views/account.php
@@ -6,8 +6,8 @@
             <div class="card border-light mb-3">
                 <div class="card-header">Informations</div>
                 <div class="card-body">
-                    <h5 class="card-title"><?php echo $full_name; ?></h5>
-                    <p class="card-text"><b>Username :</b> <?php echo $account->username; ?><br><b>Email :</b> <?php echo $account->email; ?><br><b>Last connexion :</b> <?php echo $last_connexion; ?></p>
+                    <h5 class="card-title"><?php echo $Account->getFullname(); ?></h5>
+                    <p class="card-text"><b>Username :</b> <?php echo $Account->username; ?><br><b>Email :</b> <?php echo $Account->email; ?><br><b>Last connexion :</b> <?php echo $Account->getLastConnexionFormated(); ?></p>
 
                     <a href="?url=logout" class="btn btn-danger">Logout</a>
                 </div>
