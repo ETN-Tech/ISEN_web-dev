024ad4171540f123bdba028060b70455eae62d4f
ETN-Tech <etienne.schelfhout.pro@gmail.com>
2020-11-20 17:39:31 +0100

    Added result ownership verification, added result deletion

--- php/views/account.php
+++ php/views/account.php
@@ -1,9 +1,9 @@
 <div class="content">
-    <h2>Account details</h2>
+    <h2>Accounts</h2>
     <div class="row py-2 mb-5">
         <div class="col-6">
             <div class="card">
-                <div class="card-header">Informations</div>
+                <div class="card-header">My account</div>
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $account->getFullname(); ?></h5>
                     <p class="card-text">
@@ -11,21 +11,28 @@
                         <span class="d-block"><b>Email :</b> <?php echo $account->email; ?></span>
                         <span class="d-block"><b>Last connexion :</b> <?php echo formatDate($account->last_connexion); ?></span>
                     </p>
-
                     <a href="?page=logout" class="btn btn-danger">Logout</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">Manage accounts</div>
+                <div class="card-body">
+                    <a href="?page=account-create" class="btn btn-info">Create account</a>
                 </div>
             </div>
         </div>
     </div>
 
-    <h3 id="last-scores" class="mb-3">Last quizz scores</h3>
+    <h3 id="last-scores" class="mb-3">My quizz scores</h3>
     <table class="table table-bordered table-striped">
         <thead>
             <tr>
-                <th>#</th>
                 <th>Quizz</th>
                 <th>Score</th>
                 <th>Date</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
@@ -36,10 +43,10 @@
                 $i++;
                 ?>
             <tr>
-                <th><?php echo $i; ?></th>
                 <td><a href="?page=quizz-questions&quizz=<?php echo $quizz->name; ?>" class="text-info"><?php echo $quizz->title; ?></a></td>
                 <td><a href="?page=quizz-score&date=<?php echo $date; ?>" class="text-<?php echo Quizz::getScoreType($score); ?>"><?php echo $score; ?></a></td>
                 <td><?php echo formatDate($date); ?></td>
+                <td><a href="?page=quizz-score-delete&date=<?php echo $date; ?>" class="btn btn-danger">Delete</a></td>
             </tr>
             <?php } ?>
         </tbody>
