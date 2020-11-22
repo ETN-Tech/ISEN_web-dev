db5b94e9e80fee519dadf4c3cdf4fc8c175b3127
ETN-Tech <etienne.schelfhout.pro@gmail.com>
2020-11-23 00:21:04 +0100

    Added quizz-results

---
+++ php/controllers/quizz-results.php
@@ -0,0 +1,28 @@
+<?php
+
+// verify if quizz page requested
+if (!isset($_GET['quizz']) || empty($_GET['quizz'])) {
+    header('Location: ?page=quizz');
+    die();
+}
+
+// secure quizz name
+$quizz_name = htmlspecialchars($_GET['quizz']);
+
+// verify if the quizz exists
+if (!Quizz::quizzExistByName($quizz_name)) {
+    header('Location: ?page=quizz');
+    die();
+}
+
+// get the quizz with the name
+$quizz = Quizz::getQuizzByName($quizz_name);
+
+$meta_title = "Results quizz ". $quizz->title;
+
+// get quizz questions
+$results_date = AccountAnswer::getAccountAnswerDatesByQuizzId($quizz->id);
+
+
+// view quizz questions page
+require_once('../php/views/quizz-results.php');
